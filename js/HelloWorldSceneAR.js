51c7401d2eddd9a99f45b9e56800640960071375
Andres <andresgomezjr89@gmail.com>
2020-07-06 16:47:44 -0400

    edited ar scene to use mishu component

--- js/HelloWorldSceneAR.js
+++ js/HelloWorldSceneAR.js
@@ -2,24 +2,16 @@
 
 import React, { Component } from 'react';
 
-import {StyleSheet} from 'react-native';
+import { ViroARScene, ViroAmbientLight, ViroConstants } from 'react-viro';
 
-import {
-  ViroARScene,
-  ViroText,
-  ViroConstants,
-} from 'react-viro';
+import MishuComponent from './MishuComponent'
 
 export default class HelloWorldSceneAR extends Component {
 
   constructor() {
     super();
-
     // Set initial state here
-    this.state = {
-      text : "Initializing AR..."
-    };
-
+    this.state = { };
     // bind 'this' to functions
     this._onInitialized = this._onInitialized.bind(this);
   }
@@ -27,30 +19,26 @@ export default class HelloWorldSceneAR extends Component {
   render() {
     return (
       <ViroARScene onTrackingUpdated={this._onInitialized} >
-        <ViroText text={this.state.text} scale={[.5, .5, .5]} position={[0, 0, -1]} style={styles.helloWorldTextStyle} />
+      
+        <ViroAmbientLight color={"#aaaaaa"} />
+        
+        {/* Draw our mishu component */}
+        <MishuComponent />
+      
       </ViroARScene>
     );
   }
 
   _onInitialized(state, reason) {
     if (state == ViroConstants.TRACKING_NORMAL) {
-      this.setState({
-        text : "Hello World!"
-      });
-    } else if (state == ViroConstants.TRACKING_NONE) {
+      console.log("Viro Tracking Initialized!");
+      this.setState();
+    } 
+    else if (state == ViroConstants.TRACKING_NONE) {
+      console.error("Viro Tracking Error!\n", reason);
       // Handle loss of tracking
     }
   }
 }
-
-var styles = StyleSheet.create({
-  helloWorldTextStyle: {
-    fontFamily: 'Arial',
-    fontSize: 30,
-    color: '#ffffff',
-    textAlignVertical: 'center',
-    textAlign: 'center',  
-  },
-});
 
 module.exports = HelloWorldSceneAR;
