36f2aefb50518f7bcf7c41d163842a8a60d3aa58
Andres <andresgomezjr89@gmail.com>
2020-07-07 15:17:51 -0400

    changed scale in transform to rotation

--- js/Transform.js
+++ js/Transform.js
@@ -8,19 +8,19 @@ const mishuTransform = {
     // initialize so it's in front of the camera
     position: [0,0,-1],
     
-    scale: [.1,.1,.1],
+    rotation: [0,0,0],
 
     setPosition: function(x, y, z) {
         this.position = [x,y,z];
     },
-    setScale: function(x, y, z) {
-        this.scale = [x,y,z];
+    setRotation: function(x, y, z) {
+        this.rotation = [x,y,z];
     },
     getPosition: function() {
         return [...this.position];
     },
-    getScale: function() {
-        return [...this.scale];
+    getRotation: function() {
+        return [...this.rotation];
     }
 }
 
