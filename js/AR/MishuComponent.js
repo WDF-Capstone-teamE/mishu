ee5b59f8ee14b7a356a709dce171830a73d8922a
Tiffany Sanchez <tiffanytha.sj@gmail.com>
2020-07-10 14:38:03 -0400

    added and tested icecream and turkey compoenents to make sure they were not the source of bugs introduced

--- js/AR/MishuComponent.js
+++ js/AR/MishuComponent.js
@@ -3,28 +3,83 @@
     just a wrapper around drawing a box for now
 */
 
-import React from 'react'
-
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
 import mishuTransform from './Transform'
+import { Viro3DObject, ViroMaterials } from "react-viro";
 
-import { ViroBox, ViroMaterials } from "react-viro";
+class MishuComponent extends Component {
 
-function MishuComponent () {
-    return (
-        <ViroBox
-          scale={[.1,.1,.1]}
-          position={mishuTransform.getPosition()}
-          rotation={mishuTransform.getRotation()}
-          materials={["mishuMaterial"]}
-        />
-    );
+    constructor() {
+        super();
+    }
+
+    render() {
+
+        let model = "iceCream";
+
+        if(model === "iceCream"){
+            return (
+                <Viro3DObject
+
+                source={require('./res/icecreamman_anim/icecreamman_anim_a.vrx')}
+                materials={["iceCreamMaterials"]}
+
+                position={mishuTransform.getPosition()}
+                rotation={mishuTransform.getRotation()}
+
+                scale={[.3, .3, .3]}
+
+                type="VRX"
+                onClick={this.onTap}
+                animation={{name:"01"/*this.props.currentAnimation*/, run:true, loop:true,}}
+              />
+            )
+        }
+
+        else if(model === "turkey"){
+            return (
+                <Viro3DObject
+
+                source={require('./res/turkeyman_anim/turkeyman_anim.vrx')}
+                materials={["turkeyMaterials"]}
+
+                position={mishuTransform.getPosition()}
+                rotation={mishuTransform.getRotation()}
+
+                scale={[.3, .3, .3]}
+
+                type="VRX"
+                onClick={this.onTap}
+                animation={{name:"01"/*this.props.currentAnimation*/, run:true, loop:true,}}
+              />
+            )
+        }
+
+    }
 }
 
 ViroMaterials.createMaterials({
-    mishuMaterial: {
-        diffuseTexture: require("./res/grid_bg.jpg"),
+    iceCreamMaterials: {
+        // lightingModel: "Blinn",
+        diffuseTexture: require('./res/icecreamman_anim/icecreamman_diffuse.png'),
+        // normalTexture: require('./res/icecreamman_anim/icecreamman_normal.png'),
+        // specularTexture: require('./res/icecreamman_anim/icecreamman_specular.png')
     },
+    turkeyMaterials: {
+        // lightingModel: "Blinn",
+        diffuseTexture: require('./res/turkeyman_anim/turkeyman_diffuse.jpg'),
+        // normalTexture: require('./res/turkeyman_anim/turkeyman_normal.jpg'),
+        // specularTexture: require('./res/turkeyman_anim/turkeyman_specular.jpg')
+    }
 });
   
 
-module.exports = MishuComponent;
+// module.exports = function mapStateToProps(state) {
+//     return {
+//         currentAnimation: state.current
+//     }
+// }
+
+module.exports = MishuComponent
+// module.exports = connect(mapStateToProps)(MishuComponent);
