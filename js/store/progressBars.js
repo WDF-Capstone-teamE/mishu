e4b35bb8d94ce76cb43c8a6e9d4189cf9eb625cb
Frederick Tetteh <kwesiossom@gmail.com>
2020-07-17 10:17:43 -0400

    store set up

--- js/store/progressBars.js
+++ js/store/progressBars.js
@@ -4,57 +4,89 @@
 
 const HEALTH = "HEALTH";
 const HUNGER = "HUNGER";
-const HAPPINESS ="HAPPINESS"
-const CLEANLINESS = "CLEANLINESS"
+const HAPPINESS = "HAPPINESS";
+const CLEANLINESS = "CLEANLINESS";
 /**
  * ACTION CREATORS
  */
-export const setHealth = (option, amount) => ({
+export const getHealth = () => ({
   type: HEALTH,
-  bar: {
-    option: option,
-    amount: amount
-  }
 });
 export const setHappiness = (option, amount) => ({
   type: HAPPINESS,
   bar: {
     option: option,
-    amount: amount
-  }
+    amount: amount,
+  },
 });
-export const setHunger = (option,amount) => ({
+export const setHunger = (option, amount) => ({
   type: HUNGER,
   bar: {
     option: option,
-    amount: amount
-  }
-})
-export const setCleanliness = (option,amount) => ({
+    amount: amount,
+  },
+});
+export const setCleanliness = (option, amount) => ({
   type: CLEANLINESS,
   bar: {
     option: option,
-    amount: amount
-  }
-})
+    amount: amount,
+  },
+});
 
 /**
  * THUNK CREATORS
  */
+/**
+ * INITIAL STATE
+ */
 const initialState = {
-  healthBar:50,
-  happinessBar:50,
-  hungerBar:50,
-  cleanlinessBar:50
-}
+  healthBar: 50,
+  happinessBar: 50,
+  hungerBar: 50,
+  cleanlinessBar: 50,
+};
 
-export default (state = initialState, { type, payload }) => {
-  switch (type) {
-
-  case typeName:
-    return { ...state, ...payload }
-
-  default:
-    return state
+/**
+ * REDUCER
+ */
+export default function (state = initialState, action) {
+  switch (action.type) {
+    case HEALTH: {
+      let health =
+        state.happinessBar * 0.333 +
+        state.hungerBar * 0.333 +
+        state.cleanlinessBar * 0.333 + 0.1;
+      return { ...state, healthBar: health };
+    }
+    case HAPPINESS: {
+      let happiness =
+        action.bar.option === "increase"
+          ? state.happinessBar + action.bar.amount
+          : state.happinessBar - action.bar.amount;
+      if (happiness >= 100) happiness = 100;
+      else if (happiness <= 0) happiness = 0;
+      return { ...state, happinessBar: happiness };
+    }
+    case HUNGER: {
+      let hunger =
+        action.bar.option === "increase"
+          ? state.hungerBar + action.bar.amount
+          : state.hungerBar - action.bar.amount;
+      if (hunger >= 100) hunger = 100;
+      else if (hunger <= 0) hunger = 0;
+      return { ...state, hungerBar: hunger };
+    }
+    case CLEANLINESS: {
+      let cleanliNess =
+        action.bar.option === "increase"
+          ? state.cleanliNessBar + action.bar.amount
+          : state.cleanlinessBar - action.bar.amount;
+      if (cleanliNess >= 100) cleanliNess = 100;
+      else if (cleanliNess <= 0) cleanliNess = 0;
+      return { ...state, cleanlinessBar: cleanliNess };
+    }
+    default:
+      return state;
   }
 }
